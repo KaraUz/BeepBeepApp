0b2845561d9b2f473a40df73cdb6e7b08d9a1152
Karolis Uzdanavicius <uzda.kara@gmail.com>
2014-10-24 16:55:58 +0300

    Com1

---
+++ FillActivity.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BeepBeep
+{
+	[Activity (Label = "FillActivity")]			
+	public class FillActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.FillLayout);
+
+			// Create your application here
+		}
+	}
+}
+
