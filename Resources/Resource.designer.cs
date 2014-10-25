466f6d4a1e81c647c050b72d3c6f8b0132e54a7e
Karolis Uzdanavicius <uzda.kara@gmail.com>
2014-10-25 10:00:44 +0300

    new 10:00

--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -166,6 +166,9 @@ namespace BeepBeep
 			// aapt resource value: 0x7f030001
 			public const int Main = 2130903041;
 			
+			// aapt resource value: 0x7f030002
+			public const int OptionsLayout = 2130903042;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
